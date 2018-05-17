ec2403c99658bab77dfbf43cc768186d40235fda
Sierra Kilo <sierrakilo@Sierras-MBP.webpass.net>
2018-05-16 20:29:04 -0700

    created and started using playground folder for practice problems

--- src/app.js
+++ src/app.js
@@ -2,34 +2,43 @@ console.log('app.js is running');
 
 // jsx - JavaScript XML
 
-var app = {
+const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-}
+  options: ['One', 'Two']
+};
 
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Your options' : 'No Options'}</p>
     <ol>
       <li>Item One</li>
       <li>Item Two</li>
     </ol>
   </div>
 );
-// var user = {
-//   name: 'The Dude',
-//   age: 500,
-//   location: 'Here',
-// }
-//
-// var template2 = (
-//   <div>
-//     <h1>{user.name}</h1>
-//     <p>Age: {user.age}</p>
-//     <p>Location: {user.location}</p>
-//   </div>
-// )
-var appRoot = document.getElementById('app');
+
+const user = {
+  name: 'The Dude',
+  age: 500,
+  location: 'There',
+};
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+};
+const template2 = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {user.age>= 18 && <p>Age: {user.age}</p> }
+    {/* <p>Age: {user.age}</p> */}
+    {getLocation(user.location)}
+  </div>
+);
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot)
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,15 @@
+var nameVar = 'Andrew';
+var nameVar = 'Mike';
+console.log('nameVar', nameVar)
+
+let nameLet = 'Jen'
+nameLet = 'Julie'
+console.log('nameLet', nameLet)
+
+const nameConst = 'Frank'
+console.log('nameConst', nameConst);
+
+// functioin getPetName() {
+//   var getPetName = 'hal'
+//   return getPetName
+// }
